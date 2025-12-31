b65910906876191483ab1dc3077772332b1ccf6d
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-12-31 17:13:42 +0530

    feature add Show nickname with each message and User join / leave system messages

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require("http")
 const express = require("express")
 const path = require('path')
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
+const { log } = require("console");
 
 const app = express();
 const server = http.createServer(app)
@@ -9,26 +10,30 @@ const io = new Server(server)
 
 //socket.io
 const onlineUsers = new Set();
+const users ={}
 
 io.on("connection",(socket)=>{
-    
+    console.log(socket.id);    
     onlineUsers.add("online");
     
-    socket.broadcast.emit("user-online","online")
-
-    socket.on("user-message",(message)=>{
-        io.emit("message",message)
-    })
+    socket.broadcast.emit("user-online",users[socket.id])
 
     socket.on("user-nickname",(nick)=>{
         socket.emit("nickname",nick)
+        users[socket.id] = nick
+        console.log(users);
+        
+    })
+    
+    socket.on("user-message",(message)=>{
+        io.emit("message",[message, users[socket.id]])
     })
 
     socket.emit("online-users", Array.from(onlineUsers));
 
     socket.on("disconnect", () => {
+        socket.broadcast.emit("user-offline", users[socket.id]);
         onlineUsers.delete("online");
-        socket.broadcast.emit("user-offline", "offline");
     });
 })
 
